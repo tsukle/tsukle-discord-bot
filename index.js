a9181f137ce6fa6f64e0ab4401bd0cdead266e0f
Emilis Tobulevicius <etobulevicius@hotmail.co.uk>
2017-06-21 17:24:41 +0100

    separated wordlist and cleaned up code.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const info = require('./info.json');
+const badwords = require('./badwords.json');
+const Chalk = require('chalk');
 const Discord = require('discord.js');
 const Client = new Discord.Client();
 
 //If these words are found an instant timeout is to be given.
-const bannablewordlist = ["nigger", "faggot", "nigga", "n1gga", "n1gg4", "nigg4", "paki", "nigg3r", "n1gg3r", "n1gger", "cunt", "dyke", "dyk3"];
+const bannablewordlist = badwords.words;
 const roles = {
         'Tsukle': '326831922774933504',
         'Administrators': '326832208486727680',
         'Moderators': '326832940979978241',
         'Spicy': '326831645481238531',
+        'DedicatedFollower': '327108020112719872',
         'Timeout': '326924318325866496'
       };
 
@@ -29,13 +32,12 @@ Client.on('guildMemberAdd', member => {
 
 // Message received.
 Client.on('message', message =>{
-  console.log(`${message.author.username}: ${message.content}`);
-
+  console.log(Chalk.blue.bgYellow(`${message.author.username}: ${message.content}`));
   for(i in bannablewordlist){
     if(message.content.toLowerCase().includes(bannablewordlist[i])){
-      message.author.send(`Please refrain from using words that can be seen as highly offensive.\nYou have been timed out on the server. Message ${message.member.guild.owner} to get your roles back.`);
-      message.member.guild.owner.send(`New Timeout: ${message.author}`);
-      message.member.removeRoles([roles.Spicy]);
+      message.author.send(`You have been timed out for using a flagged word. Read the rules of the server and then message ${message.member.guild.owner} to get your roles back.`);
+      message.member.guild.owner.send(`This user has been timed out for using a flagged word: ${message.author}`);
+      message.member.removeRoles([roles.Spicy, roles.DedicatedFollower]);
       message.member.addRole(roles.Timeout);
       message.delete();
     }
@@ -44,7 +46,7 @@ Client.on('message', message =>{
   if(message.content === "!twitch"){
     setTimeout(() =>{
       message.channel.send(`You can find me on Twitch over at: https://twitch.tv/tsukle ! (${message.author})`)
-        .then(msg => console.log(`Replied to: ${msg.author.username}`))
+        .then(msg => console.log(Chalk.blue.bgYellow(`Replied to: ${msg.author.username}`)))
         .catch(console.error);
     }, 500);
   }
@@ -52,7 +54,7 @@ Client.on('message', message =>{
   if(message.content === "!invite"){
     setTimeout(() => {
       message.channel.send(`Send this to your friends: https://discordapp.com/invite/QhdtEzu ! (${message.author})`)
-        .then(msg => console.log(`Replied to: ${msg.author.username}`))
+        .then(msg => console.log(Chalk.blue.bgYellow(`Replied to: ${msg.author.username}`)))
         .catch(console.error);
     }, 500);
   }
@@ -60,7 +62,7 @@ Client.on('message', message =>{
   if(message.content === "!twitter"){
     setTimeout(() => {
       message.channel.send(`Catch my tweets at: https://twitter.com/tsukle ! (${message.author})`)
-        .then(msg => console.log(`Replied to: ${msg.author.username}`))
+        .then(msg => console.log(Chalk.blue.bgYellow(`Replied to: ${msg.author.username}`)))
         .catch(console.error);
     }, 500);
   }
@@ -68,7 +70,7 @@ Client.on('message', message =>{
   if(message.content === "!website"){
     setTimeout(() => {
       message.channel.send(`Check out my website at: https://tsukle.com ! (${message.author})`)
-        .then(msg => console.log(`Replied to: ${msg.author.username}`))
+        .then(msg => console.log(Chalk.blue.bgYellow(`Replied to: ${msg.author.username}`)))
         .catch(console.error);
     }, 500);
   }
@@ -76,7 +78,7 @@ Client.on('message', message =>{
   if(message.content === "!twpy"){
     setTimeout(() => {
       message.channel.send(`Check out my twpy at: https://twpy.uk ! (${message.author})`)
-        .then(msg => console.log(`Replied to: ${msg.author.username}`))
+        .then(msg => console.log(Chalk.blue.bgYellow(`Replied to: ${msg.author.username}`)))
         .catch(console.error);
     }, 500);
   }
@@ -84,7 +86,7 @@ Client.on('message', message =>{
   if(message.content === "!commands"){
     setTimeout(() => {
       message.channel.send(`Commands:\n!twitch\n!twitter\n!website\n!twpy\n!invite\n(${message.author})`)
-        .then(msg => console.log(`Replied to: ${msg.author.username}`))
+        .then(msg => console.log(Chalk.blue.bgYellow(`Replied to: ${msg.author.username}`)))
         .catch(console.error);
     }, 500);
   }
